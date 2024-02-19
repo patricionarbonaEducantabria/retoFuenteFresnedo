20a587ab3b7dc1becb5c586be46b706b8dc7027f
PatricioNarbona <pnarbonaa01@educantabria.es>
2024-02-19 12:17:26 +0100

    añadido admin gestiones

--- JS/inicioAdmin.js
+++ JS/inicioAdmin.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("btnUsuario").onclick = function() {
-        window.location.href = "../Paginas/usuario.html";
+        window.location.href = "../Paginas/usuario/usuario_inicio.html";
     };
 
     document.getElementById("btnAdmin").onclick = function() {
-        window.location.href = "../Paginas/admin.html";
+        window.location.href = "../Paginas/admin/admin_gestion.html";
     };
 });
