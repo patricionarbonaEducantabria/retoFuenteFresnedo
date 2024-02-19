0e2367955f675cecd9fba4c7f8f4818dc191c5c1
Pablo <jhintecarrea1237@gmail.com>
2024-02-19 12:32:45 +0100

    cambios

--- JS/nuevaContrasenia.js
+++ JS/nuevaContrasenia.js
@@ -1,23 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Tu código JavaScript aquí
-    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@!%*?&#¡.])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
 
     document.getElementById("btnConfirmar").addEventListener("click", function() {
-        var contrasenia = document.getElementById("inContrasenia").value;
-        var confirmarContrasenia = document.getElementById("inconfirmarContrasenia").value;
+        let contrasenia = document.getElementById("inContrasenia").value;
+        let confirmarContrasenia = document.getElementById("inConfirmarContrasenia").value;
 
-        // Validar la nueva contraseña con la expresión regular
-        if (!regex.test(contrasenia)) {
-            alert("La nueva contraseña no cumple con los requisitos de seguridad.");
+            // Si estan rellenados empiezo a comprobar si existe los datos del usuario
+            if(contrasenia && confirmarContrasenia) {
+            // Validar la nueva contraseña con la expresión regular
+            if (!regex.test(contrasenia)) {
+                alert("La nueva contraseña no cumple con los requisitos de seguridad.");
+                return;
+            }
+
+            // Verificar si las contraseñas ingresadas coinciden
+            if (contrasenia !== confirmarContrasenia) {
+                alert("Las contraseñas ingresadas no coinciden.");
+                return;
+            }
+        }else {
+            alert("Rellena todos los campos");
             return;
         }
-
-        // Verificar si las contraseñas ingresadas coinciden
-        if (contrasenia !== confirmarContrasenia) {
-            alert("Las contraseñas ingresadas no coinciden.");
-            return;
-        }
-
         // Si todas las validaciones pasan, enviar el formulario
         document.getElementById("loginForm").submit();
     });
